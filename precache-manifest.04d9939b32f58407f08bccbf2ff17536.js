1f78338cc3e9ab5b9239c3deaf34f462f335a183
mcpengelly <pengelly.mat@gmail.com>
2019-03-11 13:18:31 -0400

    Updates

---
+++ precache-manifest.04d9939b32f58407f08bccbf2ff17536.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "775236cd6c701351f828",
+    "url": "/static/js/main.775236cd.chunk.js"
+  },
+  {
+    "revision": "a91f8d0996c9a6dd77da",
+    "url": "/static/js/2.a91f8d09.chunk.js"
+  },
+  {
+    "revision": "775236cd6c701351f828",
+    "url": "/static/css/main.a43993f9.chunk.css"
+  },
+  {
+    "revision": "52f5883e2fe9ac869a83657b638eabc4",
+    "url": "/index.html"
+  }
+];
