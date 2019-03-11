15e4f926a9f91360dfab74967980adb096feb406
mcpengelly <pengelly.mat@gmail.com>
2019-03-11 13:10:41 -0400

    Updates

---
+++ precache-manifest.454a73420cbd5b6a1bd453bd8b4554c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84e2ee1062062d301e9a",
+    "url": "/landing-page/static/js/runtime~main.84e2ee10.js"
+  },
+  {
+    "revision": "fdf45d7cfbc3a294a647",
+    "url": "/landing-page/static/js/main.fdf45d7c.chunk.js"
+  },
+  {
+    "revision": "a91f8d0996c9a6dd77da",
+    "url": "/landing-page/static/js/2.a91f8d09.chunk.js"
+  },
+  {
+    "revision": "fdf45d7cfbc3a294a647",
+    "url": "/landing-page/static/css/main.a43993f9.chunk.css"
+  },
+  {
+    "revision": "741bf9f833c8012dec6220cbfd777c11",
+    "url": "/landing-page/index.html"
+  }
+];
